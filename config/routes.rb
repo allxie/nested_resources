965ddbd206566509d01cc907111aa86da7ad1ac0
Allxie <allxiecleary@gmail.com>
2015-04-29 11:37:30 -0700

    linked pages around, added nested resource in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
-  root to: "authors#index"
+  
+  resources :authors do
+    resources :books
+  end
 
-  get "/authors" => "authors#index"
-
-  get "/authors/new" => "authors#new"
-
-  post "/authors" => "authors#create"
-
-  get "/authors/:id" => "authors#show"
-
-  get "/authors/:id/edit" => "authors#edit"
-
-  post "/authors/:id" => "authors#update"
-
-  post "/authors/:id" => "authors#destroy"
+  # root to: "authors#index"
+  # get "/authors" => "authors#index"
+  # get "/authors/new" => "authors#new"
+  # post "/authors" => "authors#create"
+  # get "/authors/:id" => "authors#show"
+  # get "/authors/:id/edit" => "authors#edit"
+  # post "/authors/:id" => "authors#update"
+  # post "/authors/:id" => "authors#destroy"
 
 
 
